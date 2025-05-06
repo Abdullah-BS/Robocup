918d7ce4292bb43195dfc426197cbc58b77e6b8e
Abdullah-BS <139412761+Abdullah-BS@users.noreply.github.com>
2025-05-07 01:29:49 +0300

    Update small_esp.ino

--- Main/small_esp.ino
+++ Main/small_esp.ino
@@ -1,7 +1,7 @@
 #include <Bluepad32.h>
 
 // Motor control definitions
-#define RXD2 16
+// #define RXD2 16
 #define TXD2 1
 
 
@@ -140,8 +140,8 @@ void processControllers() {
 }
 
 void setup() {
-  Serial.begin(9600);
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);  // UART to Arduino
+  Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);  // UART to Arduino
 
   Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
 
